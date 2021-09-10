6561d48396e921678413f3c6288c3c0c3f847819
jawwad-ali <alijawwad001@gmail.com>
2021-09-10 18:48:21 +0500

    add netlify.toml

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,10 @@ require("dotenv").config({
 })
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Operation Unicorn`,
+    author: `Ali Jawwad`,
+  },
   plugins: [
     `gatsby-transformer-remark`,
     `gatsby-plugin-image`,
